8d3000789c080d3c4e813875855f409d5a7bcbae
Pedro picapiedra <pibarrar1500@alumno.ipn.mx>
2020-11-21 01:36:10 -0600

    prueba esta

--- src/Validaciones.java
+++ src/Validaciones.java
@@ -10,7 +10,8 @@ public class Validaciones {
     //private String regexF = "(19|20)" + "[0-9]{1}[0-9]{1}" + "(-)" + "[0-1][1-9]";
     //private String regexF = "((19|20)\\d\\d)-(0[1-9]|1[012])-([012][0-9]|3[012])$";
     //private String regexF = "(((\\d\\d)(0[48]|[2468][048]|[13579][26])|([02468][048]|[13579][26])(00))(02)([012]\\d))|(\\d\\d([02468][1235679]|[13579][01345789])(02)([01]\\d|2[012345678]))|(\\d\\d\\d\\d((0[13578]|1[02])([012]\\d|3[01])|((0[46]|11)([012]\\d|30))))";
-    private String regexF = "[1, 2][0-9]{3}";
+    //private String regexF = "[1, 2][0-9]{3}";
+    private String regexF = "\\d{1,2}-\\d{1,2}-\\{4}";
     private Pattern patronF = Pattern.compile(regexF);
 
 
