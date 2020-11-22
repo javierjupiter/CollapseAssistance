27e78a30f4015c1e8410081b733cf09b9abd0b5d
javierjupiter <javierjupiters@gmail.com>
2020-11-22 00:06:13 -0600

    Validacion de fecha verifica tambien si es bisiesto el año

--- src/Validaciones.java
+++ src/Validaciones.java
@@ -1,8 +1,6 @@
 import java.util.regex.Pattern;
 
 public class Validaciones {
-
-
     private String regexCURP = "^[A-Z][AEIOU][A-Z]{2}[0-9]{2}" +
         "(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])" +
         "[HM]" +
@@ -11,13 +9,17 @@ public class Validaciones {
         "[0-9A-Z]{1}[0-9]$";
     private Pattern patronCURP = Pattern.compile(regexCURP);
 
-    private String regexFecha = "^(19[5-9][0-9]|20[0-2][0-9])-(" +  //Año en AAAA acepta de 1950 a 2029
-                            "(0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|" + //Meses con 31 días
-                            "(0[469]|11)-(0[1-9]|[12][0-9]|30)|" + //Meses con 30 días
-                            "02-(0[1-9]|[12][0-9]))$"; //Febrero
+    private String anyiobiciesto = "((19[579][26])|(19[68][048])|(201[26])|(20[02][048]))";
+    private String anyoNormal = "((19[579][01345789])|(19[68][1235679])|(201[01345789])|(20[02][1235679]))";
+    private String mesLargo = "(0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])";
+    private String mesCorto = "(0[469]|11)-(0[1-9]|[12][0-9]|30)";
+    private String febreroLargo = "02-(0[1-9]|[12][0-9])";
+    private String febreroCorto = "02-(0[1-9]|[12][0-8])";
+
+    private String regexFecha = "^(" + anyiobiciesto + "-(" + mesLargo + "|" + mesCorto + "|" + febreroLargo + "))|(" + anyoNormal + "-(" + mesLargo + "|" + mesCorto + "|" + febreroCorto + "))$";
     private Pattern patronFecha = Pattern.compile(regexFecha);
 
-    private String regexNombre = "^(([A-ZÁÉÍÓÚÑ][a-záéíóúñ]*)[ ]*)+$"; //Acepta solo con la primera letra de cada palabra con mayusculas, las siguientes deben ser minusculas
+    private String regexNombre = "^(([A-ZÁÉÍÓÚÑ][a-záéíóúñ]*+)[ ]*)+$"; //Acepta solo con la primera letra de cada palabra con mayusculas, las siguientes deben ser minusculas
     private Pattern patronNombre = Pattern.compile(regexNombre);
 
     private String regexCadenas = "^(([A-ZÁÉÍÓÚÑ][a-záéíóúñ]*)[ \\.,¿?¡!\\-_\\*\\+/]*)+$";
