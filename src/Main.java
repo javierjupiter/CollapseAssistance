e50bc45306e7ee40dd8cc2b95826c65fb32c1b68
javierjupiter <javierjupiters@gmail.com>
2020-11-27 23:26:05 -0600

    fix resolutionx3

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         Image icon = new Image(getClass().getResourceAsStream("/images/Collapse_icon.png"));
         primaryStage.getIcons().add(icon);
-        primaryStage.setScene(new Scene(root, 800, 490));
+        primaryStage.setScene(new Scene(root, 1000, 613.28));
         primaryStage.show();
     }
 
