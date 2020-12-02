73dd865f6ae72632b41894905e833b0b9789bb30
vanemalo150 <vmalob1500@alumno.ipn.mx>
2020-12-02 04:28:55 -0600

    Registro de empleados listo

--- src/ControladorUI.java
+++ src/ControladorUI.java
@@ -1,5 +1,6 @@
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import java.net.URL;
@@ -47,11 +48,73 @@ public class ControladorUI {
     private JFXButton btnGuardaRJustificantes;
     // Finaliza propiedades registro de justificantes.
 
+    //Inicia propiedades de registro de Incidencias
+    @FXML
+    private RegistroIncidenciasUI objRegistroIncidenciasUI;
+    @FXML
+    private JFXTextField txtClaveRegistroIncidencias;
+    @FXML
+    private JFXDatePicker pickerIncidenciaFechaRegistroIncidencias;
+    @FXML
+    private JFXDatePicker pickerInsercionRegistroIncidencias;
+    @FXML
+    private JFXComboBox comboTipoRegistroIncidencias;
+    @FXML
+    private JFXComboBox comboStatusRegistroIncidencias;
+    @FXML
+    private JFXButton btnGuardarRegistroIncidencias;
+    //Finaliza propiedades de registro de Incidencias
+
+    //Inicia propiedades de registro de dias laborales
+    private DiasLaboralesUI objDiasLaboralesUI;
+    @FXML
+    private JFXTextField txtMotivoLaborales;
+    @FXML
+    private JFXComboBox<String> comboStatusLaborales;
+    @FXML
+    private JFXDatePicker pickerLaboralesFechaLaborales;
+    @FXML
+    private JFXButton btnGuardarDiasLaborables;
+    //Finaliza propiedades de registro de dias laborales
+
+    //Inicia propiedades del registro de empleados
+    private RegistroEmpleadosUI objRegitroEmpleadosUI;
+    @FXML
+    private JFXTextField txtIdEmpleadoREmpleados;
+    @FXML
+    private JFXComboBox<String> comboMunicipioREmpleados;
+    @FXML
+    private JFXDatePicker pickerFechaREmpleados;
+    @FXML
+    private JFXButton btnGuardarREmpleados;
+    @FXML
+    private JFXTextField txtNombresREmpleados;
+    @FXML
+    private JFXTextField txtCurpREmpleados;
+    @FXML
+    private JFXTextField txtNumeroExtREmpleados;
+    @FXML
+    private JFXTextField txtCodigoPostalREmpleados;
+    @FXML
+    private JFXTextField txtApellidoMaternoREmpleados;
+    @FXML
+    private JFXTextField txtCalleREmpleados;
+    @FXML
+    private JFXTextField txtColoniaREmpleados;
+    @FXML
+    private JFXTextField txtApellidoPaternoREmpleados;
+    @FXML
+    private JFXTextField txtNumeroIntREmpleados;
+    @FXML
+    private JFXComboBox<String> comboEstadoREmpleados;
+    //Finaliza propiedades de registro de empleados
+
     @FXML
     public void initialize(){
+        objDiasLaboralesUI = new DiasLaboralesUI(txtMotivoLaborales,comboStatusLaborales,pickerLaboralesFechaLaborales,btnGuardarDiasLaborables);
         objHorariosUI = new HorariosUI(txtIdHorarioHorarios,txtDescripcionHorarioHorarios,txtEstatusHorarios,btnGuardarHorarios);
         objRJustificantesUI = new RegistroJustificantesUI(comboTipoRJustificantes, comboEstatusRJustificantes, txtClaveRJustificantes, txtApellidoMRJustificantes, txtNombreRJustificantes, txtApellidoPRJustificantes, btnGuardaRJustificantes);
-
-
+        objRegistroIncidenciasUI = new RegistroIncidenciasUI(txtClaveRegistroIncidencias,pickerIncidenciaFechaRegistroIncidencias,pickerInsercionRegistroIncidencias,comboTipoRegistroIncidencias,comboStatusRegistroIncidencias,btnGuardarRegistroIncidencias);
+        objRegitroEmpleadosUI = new RegistroEmpleadosUI(txtIdEmpleadoREmpleados, comboMunicipioREmpleados, pickerFechaREmpleados, btnGuardarREmpleados, txtNombresREmpleados, txtCurpREmpleados, txtNumeroExtREmpleados, txtCodigoPostalREmpleados, txtApellidoMaternoREmpleados, txtCalleREmpleados, txtColoniaREmpleados, txtApellidoPaternoREmpleados, txtNumeroIntREmpleados, comboEstadoREmpleados);
     }
 }
